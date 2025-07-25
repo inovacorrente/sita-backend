77a07210d28d0cdd67714e22ca15e68b7c2d33d5
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-07-25 10:36:06 -0300

    feat: Adiciona gerenciamento de usuários personalizados com campos de permissões e grupos.

--- app_usuarios/admin.py
+++ app_usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import UsuarioCustom
+
 # Register your models here.
+admin.site.register(UsuarioCustom)
--- app_usuarios/models.py
+++ app_usuarios/models.py
@@ -1,5 +1,6 @@
 
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
 
 from .utils import gerar_matricula_para_usuario
@@ -7,20 +8,53 @@ from .utils import gerar_matricula_para_usuario
 # Create your models here.
 
 
-class UsuarioCustom(AbstractBaseUser):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, nome_completo, matricula=None, password=None, **extra_fields):  # noqa
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, nome_completo=nome_completo,
+                          matricula=matricula, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nome_completo, password=None, **extra_fields):  # noqa
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, nome_completo, None, password, **extra_fields)  # noqa
+
+
+class UsuarioCustom(AbstractBaseUser, PermissionsMixin):
     matricula = models.CharField(max_length=50, unique=True, blank=True)
     nome_completo = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     cpf = models.CharField(max_length=11, unique=True)
     telefone = models.CharField(max_length=15, blank=True, null=True)
-    data_criacao = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='usuario_custom_set',
+        blank=True,
+        verbose_name='groups',
+        help_text='Os grupos aos quais este usuário pertence.',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='usuario_custom_set',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Permissões específicas para este usuário.',
+    )
 
     username = None  # Removendo campo username
 
     USERNAME_FIELD = 'matricula'
-    REQUIRED_FIELDS = ['email', 'nome_completo', 'matricula']
+    REQUIRED_FIELDS = ['email', 'nome_completo']
+
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = 'usuário'
