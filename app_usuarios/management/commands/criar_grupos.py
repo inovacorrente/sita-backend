2c894c332a6d22c29d81935e2e1e9fbcca774e96
MauroGutMB <mgmbarros2006@gmail.com>
2025-07-26 15:44:24 -0300

    docs: alteração em 'app_usuarios/management/commands'

--- app_usuarios/management/commands/criar_grupos.py
+++ app_usuarios/management/commands/criar_grupos.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 
@@ -7,6 +6,21 @@ class Command(BaseCommand):
     help = 'Cria os grupos padrão do sistema.'
 
     def handle(self, *args, **options):
+        """
+        Método principal executado quando o comando é chamado via terminal.
+
+        Cria grupos padrão no sistema caso ainda não existam. Os grupos são:
+        - FISCAL
+        - ATENDENTE ADMINISTRATIVO
+        - ADMINISTRADOR
+        - TAXISTA
+        - MOTOTAXISTA
+        - MOTOTRISTACONDUTOR
+
+        Para cada grupo:
+        - Se o grupo não existir, será criado e uma mensagem de sucesso será exibida.
+        - Se já existir, será exibida uma mensagem de aviso.
+        """
         grupos = [
             'FISCAL',
             'ATENDENTE ADMINISTRATIVO',
@@ -15,13 +29,17 @@ class Command(BaseCommand):
             'MOTOTAXISTA',
             'MOTOTRISTACONDUTOR',
         ]
+
+        # Itera sobre a lista de nomes de grupos
         for nome in grupos:
             grupo, criado = Group.objects.get_or_create(name=nome)
             if criado:
+                # Mensagem de sucesso para grupo recém-criado
                 self.stdout.write(
                     self.style.SUCCESS(f'Grupo "{nome}" criado.')
                 )
             else:
+                # Aviso caso o grupo já exista
                 self.stdout.write(
                     self.style.WARNING(f'Grupo "{nome}" já existe.')
                 )
